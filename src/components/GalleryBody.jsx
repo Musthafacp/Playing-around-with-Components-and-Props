228638842be01c90515e0a5e0a958c18755638fb
musthafa <musthafa.cp@kalvium.community>
2023-12-15 16:02:08 +0530

    updated

---
+++ src/components/GalleryBody.jsx
@@ -0,0 +1,19 @@
+// import { imageData } from "./ImageData"
+
+export function Body(data) {
+    let imageData = data.props
+
+    return (
+        <div>
+            {
+                imageData.map(image => {
+                    return (
+                        <div key={image.id} className="column">
+                            <img src={image.img} alt=""></img>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
